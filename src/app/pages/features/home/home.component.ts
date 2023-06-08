4a71dc647a321fff32e599c051ae27bbb382ff5c
Reuben <reubenmarinduque@gmail.com>
2023-06-08 13:13:04 +0800

    sync to live

--- src/app/pages/features/home/home.component.ts
+++ src/app/pages/features/home/home.component.ts
@@ -22,7 +22,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { CalendarPickerDialogComponent } from 'src/app/component/calendar-picker-dialog/calendar-picker-dialog.component';
 import { SlideInterface } from 'src/app/shared/image-slider/types/slide.interface';
 import { DeviceDetectorService } from 'ngx-device-detector';
-
+import { MatTableDataSource } from "@angular/material/table";  
+import { ReservationService } from 'src/app/core/services/reservation.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 
 export type ChartOptions = {
@@ -50,6 +52,13 @@ export class HomeComponent implements OnInit {
   isLoading;
   user: LoginResult;
 
+  dataSource = new MatTableDataSource<any>();
+  customerDataSource = new MatTableDataSource<any>();
+  staffDataSource = new MatTableDataSource<any>();
+
+  isAdvanceSearch = false;
+  isLoadingFilter = false;
+
   summary: any[];
   vetSummary: any[];
   slides: SlideInterface[] = [
@@ -60,15 +69,48 @@ export class HomeComponent implements OnInit {
   constructor(
     private deviceService: DeviceDetectorService,
     private storageService: StorageService,
+    private reservationService: ReservationService,
+    private userService: UserService,
     private dialog: MatDialog,
     private snackBar: Snackbar
   ) {
     this.user = this.storageService.getLoginUser();
+    this.getReservations();
+    this.getCustomers();
+    this.getStaff();
   }
 
   ngOnInit(): void {
     const device = this.deviceService.getDeviceInfo();
     this.storageService.saveDeviceInfo(JSON.stringify(device));
+  }
+
+  async getReservations() {
+    this.isLoading = true;
+    await this.reservationService.getByAdvanceSearch({
+      isAdvance: this.isAdvanceSearch,
+    })
+      .subscribe(async res => {
+        if (res.success)
+          this.dataSource.data = res.data.length > 0 ? res.data.map((d) => { }) : [];
+      }, async (err) => {
+        this.isLoading = false;
+      });
+
+  }
+
+  async getCustomers() {
+    this.userService.getCustomers().subscribe(async res => {
+      (res.success)
+      this.customerDataSource.data = res.data.length > 0 ? res.data.map((d) => { }) : [];
+    });
+  }
+
+  async getStaff() {
+    this.userService.getStaff().subscribe(async res => {
+      (res.success)
+      this.staffDataSource.data = res.data.length > 0 ? res.data.map((d) => { }) : [];
+    });
   }
 
 }
